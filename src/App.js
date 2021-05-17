90b61db2615ce241cb5ecce010680a12c0727b5a
nenu4142 <nenu4142@gmail.com>
2021-05-17 20:40:07 +0530

    waves music app

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useRef } from "react";
+import "./styles/app.scss";
+import Player from "./components/player";
+import Song from "./components/song";
+import data from "./data";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+function App() {
+  const audioRef = useRef(null);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  };
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    songs[currentIndex].active = false;
+    songs[(currentIndex + 1) % songs.length].active = true;
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,115 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Agar Tum Saath Ho",
+      artist: "A. R. Rahman",
+      cover:
+        "https://pagalfree.com/images/128Agar%20Tum%20Saath%20Ho%20-%20Tamasha%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Agar Tum Saath Ho - Tamasha 128 Kbps.mp3",
+      color: ["#BC702F", "#FAFBD7"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Khulke Jeene Ka",
+      artist: "A.R. Rahman, Arijit Singh, Shashaa Tirupati",
+      cover:
+        "https://pagalfree.com/images/192Khulke%20Jeene%20Ka%20-%20Dil%20Bechara%20192%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/192-Khulke Jeene Ka - Dil Bechara 192 Kbps.mp3",
+      color: ["#8A221E", "#C7AA5C"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lakshya",
+      artist: "Shankar Mahadevan, Shankar-Ehsaan-Loy",
+      cover:
+        "https://pagalfree.com/images/128Lakshya%20-%20Lakshya%20128%20Kbps.jpg",
+      audio: "https://pagalfree.com/musics/128-Lakshya - Lakshya 128 Kbps.mp3",
+      color: ["#333E3A", "	#FFD700"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Breathless",
+      artist: "Shankar Mahadevan",
+      cover: "https://api.pendu-jatt.com/thumbmed/1065075.jpg",
+      audio: "https://dns4.vippendu.com/download/128k-hvdh/Breathless.mp3",
+      color: ["#333E3A", "#FF0000"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Jab Tak",
+      artist: "Armaan Malik",
+      cover:
+        "https://pagalfree.com/images/128Jab%20Tak%20-%20M.S.%20Dhoni%20-%20The%20Untold%20Story%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Jab Tak - M.S. Dhoni - The Untold Story 128 Kbps.mp3",
+      color: ["#BCBB6B", "#FF7F50"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Apna Time Aayega",
+      artist: "Ranveer Singh, Dub Sharma, DIVINE",
+      cover:
+        "https://pagalfree.com/images/128Apna%20Time%20Aayega%20-%20Gully%20Boy%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Apna Time Aayega - Gully Boy 128 Kbps.mp3",
+      color: ["#F7F4E3", "#4C170F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "O Saathi",
+      artist: "Atif Aslam",
+      cover:
+        "https://pagalfree.com/images/128O%20Saathi%20-%20Baaghi%202%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-O Saathi - Baaghi 2 128 Kbps.mp3",
+      color: ["#AB3B3C", "#DBC270"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ae Watan (Male)",
+      artist: "Arijit Singh",
+      cover:
+        "https://pagalfree.com/images/128Ae%20Watan%20(Male)%20-%20Raazi%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Ae Watan (Male) - Raazi 128 Kbps.mp3",
+      color: ["#876B7B", "#8FC7DF"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nachdi Phira",
+      artist: "Meghna Mishra",
+      cover:
+        "https://pagalfree.com/images/128Nachdi%20Phira%20-%20Secret%20Superstar%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Nachdi Phira - Secret Superstar 128 Kbps.mp3",
+      color: ["#2C3261", "#8B3947"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Besabriyaan",
+      artist: "Armaan Malik",
+      cover:
+        "https://pagalfree.com/images/128Besabriyaan%20-%20M.S.%20Dhoni%20-%20The%20Untold%20Story%20128%20Kbps.jpg",
+      audio:
+        "https://pagalfree.com/musics/128-Besabriyaan - M.S. Dhoni - The Untold Story 128 Kbps.mp3",
+      color: ["#5B8A61", "#AF6C58"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
